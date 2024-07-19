0041abc82b1a98f0b89b41dbd27943e3bebb6339
Ashish Waghmare <106365630+ashishh25@users.noreply.github.com>
2024-07-19 22:55:58 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,39 @@
+# Creating list
+my_list = [1, 2, 3, 4, 5]
+
+# Adding items
+my_list.append(6)
+
+# Removing items
+my_list.remove(3)
+
+# Modifying list
+my_list[0] = 10
+print(my_list)
+print(type(my_list))
+
+# Creating dictionary
+my_dict = {'name': 'John', 'age': 21, 'city': 'Delhi'}
+
+# Adding items
+my_dict['gender'] = 'Male'
+
+# Removing items
+del my_dict['age']
+
+# Updating items
+my_dict['city'] = 'Mumbai'
+print(my_dict)
+print(type(my_dict))
+
+# Creating set
+my_set = {1,2,3,4,5}
+
+# Adding items
+my_set.add(6)
+print(my_set)
+
+# Removing items
+my_set.remove(3)
+print(my_set)
+print(type(my_set))
